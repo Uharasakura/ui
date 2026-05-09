f9c6e08a358088bfc224a45748f07eebfc19f288
Uharasakura <3538352833@qq.com>
2026-05-10 02:39:41 +0800

    Refactor theme switching logic into a separate function

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import { eventSource, event_types } from '../../../script.js';
+(function() {
+  function trySwitch() {
+    const select = document.querySelector('#themes');
+    if (!select || select.options.length === 0) return false;
+    const options = select.options;
+    const random = options[Math.floor(Math.random() * options.length)];
+    console.log('随机主题：', random.value);
+    select.value = random.value;
+    select.dispatchEvent(new Event('change'));
+    return true;
+  }
 
-eventSource.on(event_types.APP_READY, () => {
-  const select = document.querySelector('#themes');
-  if (!select || select.options.length === 0) return;
-  const options = select.options;
-  const random = options[Math.floor(Math.random() * options.length)];
-  console.log('随机主题：', random.value);
-  select.value = random.value;
-  select.dispatchEvent(new Event('change'));
-});
+  function waitAndSwitch(retries = 20) {
+    if (trySwitch()) return;
+    if (retries <= 0) return console.warn('随机主题：找不到 #themes，放弃');
+    setTimeout(() => waitAndSwitch(retries - 1), 500);
+  }
+
+  waitAndSwitch();
+})();
